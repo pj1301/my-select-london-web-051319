735d09921be29d44de76dc63fd45894f439f3a72
James Hayward <james.hayward6@icloud.com>
2019-04-24 09:12:03 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  i = 0
  while i < collection.size
    if yield(collection[i])
-     array.push yield(collection[i])
+     array.push(yield(collection[i]))
    i += 1
  end
  array
