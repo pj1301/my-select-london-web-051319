3ecc1c27dd0db05c1fd18a5111c4a098baf7abf7
James Hayward <james.hayward6@icloud.com>
2019-04-24 09:14:40 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
  array = []
  i = 0
  while i < collection.size
-   if (yield(collection[i]))
+   if (yield(collection[i])) #again, the block is provided inside the spec file, you just need to create the conditions inside the working file
      array.push(collection[i])
    end
    i += 1
